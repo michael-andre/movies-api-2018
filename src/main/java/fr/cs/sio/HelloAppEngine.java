2949997a82a6b8c7c98d75d97c2cdd35ca52bcf4
michael-andre <michael.andre.c@gmail.com>
2018-10-29 09:05:22 +0100

    Initial commit

---
+++ src/main/java/fr/cs/sio/HelloAppEngine.java
@@ -0,0 +1,30 @@
+package fr.cs.sio;
+
+import com.google.appengine.api.utils.SystemProperty;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "HelloAppEngine", value = "/hello")
+public class HelloAppEngine extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+    response.setContentType("text/plain");
+    response.getWriter().println("Hello world");
+  }
+
+  public static String getInfo() {
+    return "Version: " + System.getProperty("java.version")
+          + " OS: " + System.getProperty("os.name")
+          + " User: " + System.getProperty("user.name");
+  }
+
+}
